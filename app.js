04cc2fed4f7827c4e26a9c3e79764a3ffefdc21c
Aaron Ramos Valencia <Aaron.Ramos@exprogroup.com>
2023-02-12 09:11:38 +0000

    bcryptHash added

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -56,30 +57,35 @@ app.get("/register", function(req, res) {
 
 app.post("/register", function(req, res) {
 
-  const newUser = new User({
-    email: req.body.username,
-    password: md5( req.body.password)
-  });
 
-  newUser.save(function(err) {
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
 
-    if (err) {
+    newUser.save(function(err) {
 
-      console.log(err);
+      if (err) {
 
-    } else {
+        console.log(err);
 
-      res.render("secrets");
-    }
+      } else {
 
-  });
+        res.render("secrets");
+      }
+
+    });
+});
+
+
 
 });
 
 app.post("/login", function(req, res) {
 
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({
     email: username
@@ -92,10 +98,16 @@ app.post("/login", function(req, res) {
 
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+  if(result === true){
 
-          res.render("secrets");
-        }
+res.render("secrets");
+
+  }
+});
+
+
+
       }
 
     }
